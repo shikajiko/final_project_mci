b8b36374224e823aab92c63f2b054cd634d4b522
shikajiko <shikajiko@github.com>
2026-06-07 18:59:43 +0700

    fix typo on column name under product table

--- clickhouse/01_raw.sql
+++ clickhouse/01_raw.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS raw.orders (
-    order_id Nullable(String),
+    order_id String,
     customer_id Nullable(String),
     order_status Nullable(String),
     order_purchase_timestamp Nullable(String),
@@ -12,17 +12,17 @@ ENGINE = MergeTree
 ORDER BY order_id;
 
 CREATE TABLE IF NOT EXISTS raw.order_payments (
-    order_id Nullable(String),
+    order_id String,
     payment_sequential Nullable(String),
     payment_type Nullable(String),
     payment_installments Nullable(String),
     payment_value Nullable(String)
 )
 ENGINE = MergeTree
-ORDER BY (order_id, payment_sequential);
+ORDER BY (order_id);
 
 CREATE TABLE IF NOT EXISTS raw.customers (
-    customer_id Nullable(String),
+    customer_id String,
     customer_unique_id Nullable(String),
     customer_zip_code_prefix Nullable(String),
     customer_city Nullable(String),
@@ -32,7 +32,7 @@ ENGINE = MergeTree
 ORDER BY customer_id;
 
 CREATE TABLE IF NOT EXISTS raw.geolocation (
-    geolocation_zip_code_prefix Nullable(String),
+    geolocation_zip_code_prefix String,
     geolocation_lat Nullable(String),
     geolocation_lng Nullable(String),
     geolocation_city Nullable(String),
@@ -42,8 +42,8 @@ ENGINE = MergeTree
 ORDER BY geolocation_zip_code_prefix;
 
 CREATE TABLE IF NOT EXISTS raw.order_items (
-    order_id Nullable(String),
-    order_item_id Nullable(String),
+    order_id String,
+    order_item_id String,
     product_id Nullable(String),
     seller_id Nullable(String),
     shipping_limit_date Nullable(String),
@@ -54,7 +54,7 @@ ENGINE = MergeTree
 ORDER BY (order_id, order_item_id);
 
 CREATE TABLE IF NOT EXISTS raw.order_reviews (
-    review_id Nullable(String),
+    review_id String,
     order_id Nullable(String),
     review_score Nullable(String),
     review_comment_title Nullable(String),
@@ -66,10 +66,10 @@ ENGINE = MergeTree
 ORDER BY review_id;
 
 CREATE TABLE IF NOT EXISTS raw.products (
-    product_id Nullable(String),
+    product_id String,
     product_category_name Nullable(String),
-    product_name_length Nullable(String),
-    product_description_length Nullable(String),
+    product_name_lenght Nullable(String),
+    product_description_lenght Nullable(String),
     product_photos_qty Nullable(String),
     product_weight_g Nullable(String),
     product_length_cm Nullable(String),
@@ -79,7 +79,7 @@ ENGINE = MergeTree
 ORDER BY product_id;
 
 CREATE TABLE IF NOT EXISTS raw.sellers (
-    seller_id Nullable(String),
+    seller_id String,
     seller_zip_code_prefix Nullable(String),
     seller_city Nullable(String),
     seller_state Nullable(String)
@@ -88,7 +88,7 @@ ENGINE = MergeTree
 ORDER BY seller_id;
 
 CREATE TABLE IF NOT EXISTS raw.product_category_translation (
-    product_category_name Nullable(String),
+    product_category_name String,
     product_category_name_english Nullable(String)
 )
 ENGINE = MergeTree
